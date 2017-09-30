f883099cdbb6969b0111b42f57a9cf428726b31a
Ian Deans <ideans715@gmail.com>
2017-09-30 10:59:07 -0700

    More Truffle playing.

---
+++ truffle_project/contracts/HelloWorld.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.4;
+
+contract HelloWorld {
+    
+    uint public balance;
+    
+    function HelloWorld() {
+        balance = 1000;
+    }
+
+    function deposit(uint _value) returns(uint _newValue) {
+        balance += _value;
+        return balance;
+    }
+}
